8d3dfb264601a888c1b91aad47ec3f271676b1b8
Scott Litz <scott.litz@gmail.com>
2019-01-19 16:17:22 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <h1 className="App-title">QUOTE BUILDER</h1>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
